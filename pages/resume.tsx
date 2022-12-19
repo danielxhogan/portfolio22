e7aa67a5aec644d116e3ca90d2ef0462b0d7e3fd
danielxhogan <danielxhogan@gmail.com>
2022-12-19 14:29:21 -0800

    add blank resume page with SideNav

---
+++ pages/resume.tsx
@@ -0,0 +1,18 @@
+import SideNav from '../components/SideNav'
+
+import Head from 'next/head'
+
+import styles from '../styles/Resume.module.css'
+
+export default function Resume() {
+  return (
+    <>
+      <Head>
+        <title>danielxhogan | resume</title>
+      </Head>
+      <main className={styles['main']}>
+        <SideNav />
+      </main>
+    </>
+  )
+}
