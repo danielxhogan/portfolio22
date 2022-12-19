3161d794b1d478e479c275bd8de389e1191a96a7
danielxhogan <danielxhogan@gmail.com>
2022-12-19 14:27:21 -0800

    initial homepage

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script src="https://kit.fontawesome.com/e856074999.js" async crossOrigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
