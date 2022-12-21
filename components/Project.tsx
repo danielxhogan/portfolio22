12a27b1c4a7de93aa41ed2fe566bd14ef4d5490c
danielxhogan <danielxhogan@gmail.com>
2022-12-20 16:41:40 -0800

    add project content, activate chevron

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,17 +1,26 @@
-import { useState } from 'react';
+import styles from '../styles/Project.module.css'
 
+import { useState } from 'react';
+import Link from 'next/link'
+import Image from 'next/image'
 import { StaticImageData } from 'next/image';
 
-import styles from '../styles/Project.module.css'
 
 const SHOWN = 'shown';
 const HIDDEN = 'hidden';
+
+type stringOrNull = String | null
 
 export interface IProject {
   key: React.Key,
   projectTitle: String,
   projectDescription: String,
-  projectImage: StaticImageData
+  projectDescriptionFull: String,
+  projectImage: StaticImageData,
+  languages: String,
+  libraries: String,
+  githubLink: String,
+  siteLink?: String,
 }
 
 interface IProjectProps {
@@ -30,11 +39,49 @@ export default function Project({ project }: IProjectProps) {
   return (
     <div className={styles['main']}>
       <div className={styles['header']}>
-        <h3>{ project.projectTitle }</h3>
-        <div className={styles['chevron']}>
+
+        <Link href={`/project/${project.key}`}>
+          <h3>{ project.projectTitle }</h3>
+        </Link>
+
+        <div
+          className={`
+            ${styles['chevron']}
+            ${styles[shownHidden]}
+          `}
+          onClick={onClickChevronBtn}
+          >
           <i className='fa-solid fa-angle-down' />
         </div>
+
       </div>
+      <Link href={`/project/${project.key}`}>
+      <div className={`
+        ${styles['content']}
+        ${styles[shownHidden]}
+        `}>
+
+        <div className={styles['image']}>
+          <Image
+            className={styles['large-image']}
+            src={project.projectImage}
+            alt='mvplaylist'
+            height={200}
+          />
+          <Image
+            className={styles['small-image']}
+            src={project.projectImage}
+            alt='mvplaylist'
+            height={100}
+          />
+        </div>
+
+        <div className={styles['description']}>
+          <h4>{ project.projectDescription }</h4>
+        </div>
+
+      </div>
+      </Link>
     </div>
   )
 }
