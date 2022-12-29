bcd535322c89396eee265a3ad60b33f08f2797c4
danielxhogan <danielxhogan@gmail.com>
2022-12-29 11:46:27 -0800

    add full description for card counter pro

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -59,7 +59,24 @@ If a user that is not the admin \
 attempts to access the admin page, they are redirected to the home page.\n \
 '
 
-const cardCounterPRODescriptionFull = ''
+const cardCounterPRODescriptionFull = '\
+Card Counter PRO is a Black Jack Game with features built in to help users learn how to count cards. \
+The game constantly monitors the hand of the user and the dealer and makes a suggestion to the \
+user on whether to hit, stay, double down, or split if possible. These suggestions are based on \
+basic strategy which is a statistically proven method of maximizing your chance of winning any \
+possible hand you can be dealt. Even when a player plays perfect basic strategy, however, they \
+only decrease the house edge to 0.5, meaning you will still lose on average.\n\n In order to deal \
+with this, the goal of counting cards is to ensure that even if the player loses more often \
+than they win, when they win, they win bigger. Counting cards determines the times when the player \
+is more likey to win or when the dealer is more likey to win. When the count is up, the player \
+is more likey to win so they will bet higher. When the count is down, the player will bet lower. \
+The count is based on what cards are drawn from the deck. More high cards in the deck are generally \
+considered better for the player. Therefore, when high cards are drawn, the count is decreased. \
+Low cards are generally considered better for the dealer. Therefore, when low cards are drawn, the \
+count is increased. The game keeps track of the count, starting at 0 and increases the count by 1 \
+any time a 2 - 6 value card is dealt. It decreases the count by 1 any time a 10 value card or an ace \
+is drawn. \
+'
 
 const portfolioDescriptionFull = ''
 
@@ -98,6 +115,7 @@ const webDevProjects = [
     languages: 'Javascript, HTML, CSS',
     librariesAndFrameworks: 'Bootstrap',
     githubLink: 'https://github.com/danielxhogan/card-counter-pro',
+    siteLink: 'https://danielxhogan.github.io/card-counter-pro/'
   },
   {
     key: 3,
